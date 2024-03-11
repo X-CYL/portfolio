d0af29f4cab98bf6b746c632369f5843fd854a79
X-CYL <castellastro@gmail.com>
2024-03-11 20:07:20 +0100

    commit last desktop dev

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -3,7 +3,9 @@ import Route from "./route.js";
 //Définir ici vos routes
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
+  new Route("/about", "About me", "./pages/about.html"),
+  new Route("/contact", "Contactez moi", "./pages/contact.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
-export const websiteName = "emat-documentation";
+export const websiteName = "x-cyl portfolio";
