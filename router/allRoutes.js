33069979b1b32a110fabd98580aa98afa0a7c9f9
X-CYL <castellastro@gmail.com>
2024-03-13 21:34:50 +0100

    mobileFirst branch rebased

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -3,8 +3,13 @@ import Route from "./route.js";
 //Définir ici vos routes
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
+<<<<<<< HEAD
   new Route("/about", "A propos", "/pages/about.html"),
   new Route("/contact", "Contactez moi", "/pages/contact.html"),
+=======
+  new Route("/about", "About me", "./pages/about.html"),
+  new Route("/contact", "Contactez moi", "./pages/contact.html"),
+>>>>>>> d0af29f (commit last desktop dev)
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
